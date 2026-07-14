5f28a809db5e2128864e2c34cff71e89325d14e5
unknown <blackhard0016@gmail.com>
2026-07-13 21:57:50 -0300

    backend con redes

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,11 @@ try {
     await db.authenticate();
     console.log('Base de datos conectada');
 
-    app.listen(process.env.PORT || 3000, () => {
+   /* app.listen(process.env.PORT || 3000, () => {
         console.log(`Servidor corriendo en puerto ${process.env.PORT || 3000}`);
+    });*/
+        app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', () => {
+        console.log(`Servidor corriendo en http://${process.env.HOST || '0.0.0.0'}:${process.env.PORT || 3000}`);
     });
 
 } catch (error) {
